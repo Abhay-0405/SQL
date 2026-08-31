2a72264d073191845ccbc31b185cc0030667755f
Abhay-0405 <tonystarkisbest27@gmail.com>
2026-08-31 08:55:05 +0530

    31Aug

---
+++ joins/570.sql
@@ -0,0 +1,9 @@
+SELECT e1.name
+FROM Employee e1
+JOIN (
+    SELECT managerId, COUNT(*) AS report_count
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) e2 ON e1.id = e2.managerId;
