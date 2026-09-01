e76f2f6846e041b1670f6a6813169562909bd107
Abhay-0405 <tonystarkisbest27@gmail.com>
2026-09-01 13:50:09 +0530

    alternate solution

--- joins/570.sql
+++ joins/570.sql
@@ -1,9 +1,6 @@
-SELECT e1.name
-FROM Employee e1
-JOIN (
-    SELECT managerId, COUNT(*) AS report_count
-    FROM Employee
-    WHERE managerId IS NOT NULL
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
-) e2 ON e1.id = e2.managerId;
+# Write your MySQL query statement below
+select m.name from
+Employee e join Employee m
+on e.managerId=m.id
+group by e.managerId 
+having count(e.id) >=5
