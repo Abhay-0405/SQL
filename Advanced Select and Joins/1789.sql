fabcb0980722e1f956be1b8315967bcad5573478
Abhay-0405 <tonystarkisbest27@gmail.com>
2026-09-23 15:29:10 +0530

    new folder pushing

---
+++ Advanced Select and Joins/1789.sql
@@ -0,0 +1,14 @@
+-- Primary department for each employee
+-- Write your MySQL query statement below
+select employee_id , department_id 
+from Employee 
+where primary_flag='Y'
+
+union 
+
+select employee_id,department_id 
+from Employee 
+group by employee_id 
+having count(department_id)=1;
+
+
---
+++ Advanced Select and Joins/180.sql
@@ -0,0 +1,27 @@
+-- consecutive numbers 
+-- # Write your MySQL query statement below
+with Rankedlogs as  (
+
+select 
+
+num , 
+lead (num,1) over (order by id ) as next_num,
+lead (num,2) over (order by id ) as next_next_num
+
+from 
+Logs
+
+ )
+
+select distinct num as consecutiveNums from Rankedlogs
+where 
+
+    num=next_num and num=next_next_num
+
+
+
+
+
+
+
+
